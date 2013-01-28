37b86198117ee5a92e614e1fe3725bafae374700
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-01-28 14:30:11 -0500

    rename tests for easier later (re-)ordering

---
+++ desktop/compilers/ansic/tests/0003.c
@@ -0,0 +1,12 @@
+/* Here's a typedef! */
+typedef char CH;
+int a, ***c;
+unsigned long long int ** b;
+double f;
+CH ch;
+
+void foo(void)
+{
+    int foo1 = 19;
+    a = 42;
+}
---
+++ desktop/compilers/ansic/tests/0005.c
@@ -0,0 +1,21 @@
+unsigned int    a, b;
+struct
+{
+    int             elem;
+}               c, d;
+
+struct A
+{
+    int             elem;
+}               e, f;
+
+struct A        g, h;
+
+struct B
+{
+    int             elem;
+};
+
+enum { e1 }     enum1;
+enum Enum2 { e1 } enum2;
+enum Enum2      enum3;
