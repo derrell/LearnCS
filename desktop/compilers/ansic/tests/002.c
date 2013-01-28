7672036a834dcab9fe917787362a922f295c98d5
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-01-28 14:26:20 -0500

    renamed test 002 to 003; created new, simpler 002

--- desktop/compilers/ansic/tests/002.c
+++ desktop/compilers/ansic/tests/002.c
@@ -1,21 +1,10 @@
 typedef int *   IntPtr;
 IntPtr          pInt;
+unsigned int    x, y;
 int             universe[2] = { 42, 23 };
+
+typedef int     Int;
+int             Int;
 
 const int       NUM = 4;
 
-struct Data1
-{
-    int             i;
-    char            c;
-    IntPtr          pInt;
-} data, *pData;
-typedef struct Data1 Data_t;
-
-struct Data1 data1;
-
-typedef struct Data2
-{
-    int             i;
-    char            c;
-} Data2;
