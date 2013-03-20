0610aee2f47f20d25bffd3ab944c38c8ecf915a8
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-03-20 15:59:29 -0400

    corrected syntax of sizeof test

--- desktop/compilers/ansic/c-tests/0050.c
+++ desktop/compilers/ansic/c-tests/0050.c
@@ -1,37 +1,48 @@
 int main(int argc, char * argv[])
 {
-    char            ch;
+    char            c;
     short           s;
     int             i;
     long            l;
     float           f;
     double          d;
-    unsigned char   uch;
+    unsigned char   uc;
     unsigned short  us;
     unsigned int    ui;
     unsigned long   ul;
 
-    printf("sizeof(char) = " + sizeof(char));
-    printf("sizeof(short) = " + sizeof(short));
-    printf("sizeof(int) = " + sizeof(int));
-    printf("sizeof(long) = " + sizeof(long));
-    printf("sizeof(float) = " + sizeof(float));
-    printf("sizeof(double) = " + sizeof(double));
-    printf("sizeof(unsigned char) = " + sizeof(unsigned char));
-    printf("sizeof(unsigned short) = " + sizeof(unsigned short));
-    printf("sizeof(unsigned int) = " + sizeof(unsigned int));
-    printf("sizeof(unsigned long) = " + sizeof(unsigned long));
+    printf("sizeof(char) = %d\n", sizeof(char));
+    printf("sizeof(short) = %d\n", sizeof(short));
+    printf("sizeof(int) = %d\n", sizeof(int));
+    printf("sizeof(long) = %d\n", sizeof(long));
+    printf("sizeof(float) = %d\n", sizeof(float));
+    printf("sizeof(double) = %d\n", sizeof(double));
+    printf("sizeof(unsigned char) = %d\n", sizeof(unsigned char));
+    printf("sizeof(unsigned short) = %d\n", sizeof(unsigned short));
+    printf("sizeof(unsigned int) = %d\n", sizeof(unsigned int));
+    printf("sizeof(unsigned long) = %d\n", sizeof(unsigned long));
 
-    printf("sizeof(char *) = " + sizeof(char *));
-    printf("sizeof(short *) = " + sizeof(short *));
-    printf("sizeof(int *) = " + sizeof(int *));
-    printf("sizeof(long *) = " + sizeof(long *));
-    printf("sizeof(float *) = " + sizeof(float *));
-    printf("sizeof(double *) = " + sizeof(double *));
-    printf("sizeof(unsigned char *) = " + sizeof(unsigned char *));
-    printf("sizeof(unsigned short *) = " + sizeof(unsigned short *));
-    printf("sizeof(unsigned int *) = " + sizeof(unsigned int *));
-    printf("sizeof(unsigned long *) = " + sizeof(unsigned long *));
+    printf("sizeof(char *) = %d\n", sizeof(char *));
+    printf("sizeof(short *) = %d\n", sizeof(short *));
+    printf("sizeof(int *) = %d\n", sizeof(int *));
+    printf("sizeof(long *) = %d\n", sizeof(long *));
+    printf("sizeof(float *) = %d\n", sizeof(float *));
+    printf("sizeof(double *) = %d\n", sizeof(double *));
+    printf("sizeof(unsigned char *) = %d\n", sizeof(unsigned char *));
+    printf("sizeof(unsigned short *) = %d\n", sizeof(unsigned short *));
+    printf("sizeof(unsigned int *) = %d\n", sizeof(unsigned int *));
+    printf("sizeof(unsigned long *) = %d\n", sizeof(unsigned long *));
+
+    printf("sizeof(c) = %d\n", sizeof(c));
+    printf("sizeof(s) = %d\n", sizeof(s));
+    printf("sizeof(i) = %d\n", sizeof(i));
+    printf("sizeof(l) = %d\n", sizeof(l));
+    printf("sizeof(f) = %d\n", sizeof(f));
+    printf("sizeof(d) = %d\n", sizeof(d));
+    printf("sizeof(uc) = %d\n", sizeof(uc));
+    printf("sizeof(us) = %d\n", sizeof(us));
+    printf("sizeof(ui) = %d\n", sizeof(ui));
+    printf("sizeof(ul) = %d\n", sizeof(ul));
 
     return 0;
 }
