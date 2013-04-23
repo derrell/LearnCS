8054e8ceb723270d88a79621891519676f3a7120
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-04-23 13:32:23 -0400

    p008 and p009

---
+++ desktop/compilers/ansic/canning/p008-horizontal-line.c
@@ -0,0 +1,54 @@
+/*
+ * Derrell Lipman
+ * Computing 1, Problem 8
+ * One Horizontal Line of Asterisks
+ */
+
+/**
+ * Main program entry point
+ * 
+ * @param argc
+ *   The number of command-line arguments to this function
+ * 
+ * @param argv
+ *   Array of command-line arguments to this function, of length argc.
+ * 
+ * @return
+ *   zero upon success, non-zero upon failure
+ */
+int main(int argc, char * argv[])
+{
+    int             i;          // loop index
+    int             conv;       // number of conversions
+    int             numStars;   // number of stars to print, read from file
+    void *          hFile;      // file handle
+
+    // Open the input file
+    hFile = fopen("/canning/p008-input", "r");
+    if (hFile == (void *) -1)
+    {
+        printf("Could not open input file\n");
+        return 1;
+    }
+
+    // Retrieve a number from the file
+    conv = fscanf(hFile, "%d", &numStars);
+
+    // Ensure we successfully read
+    if (conv != 1)
+    {
+        printf("Read from file failed.");
+        fclose(hFile);
+        return 1;
+    }
+
+    // Print out the specified number of asterisks
+    for (i = 0; i < numStars; i++)
+    {
+        printf("*");
+    }
+
+    // We've printed a complete line of asterisks. Add a newline.
+    printf("\n");
+    return 0;
+}
---
+++ desktop/compilers/ansic/canning/p009-using-for-loop.c
@@ -0,0 +1,56 @@
+/*
+ * Derrell Lipman
+ * Computing 1, Problem 9
+ * Using a For Loop
+ */
+
+/**
+ * Main program entry point
+ * 
+ * @param argc
+ *   The number of command-line arguments to this function
+ * 
+ * @param argv
+ *   Array of command-line arguments to this function, of length argc.
+ * 
+ * @return
+ *   zero upon success, non-zero upon failure
+ */
+int main(int argc, char * argv[])
+{
+    int             i;          // loop index
+    int             conv;       // number of conversions
+    int             value;      // the value read from the file
+    void *          hFile;      // file handle
+
+    // Open the input file
+    hFile = fopen("/canning/p009-input", "r");
+    if (hFile == (void *) -1)
+    {
+        printf("Could not open input file\n");
+        return 1;
+    }
+
+    // For each of the five values in the file...
+    for (i = 0; i < 5; i++)
+    {
+        // ... retrieve a number from the file
+        conv = fscanf(hFile, "%d", &value);
+
+        // Ensure we successfully read
+        if (conv != 1)
+        {
+            printf("Read of number %d from file failed.", i);
+            fclose(hFile);
+            return 1;
+        }
+
+        // Print the number
+        printf("%d ", value);
+    }
+
+    // We've printed all of the numbers. Terminate the line.
+    printf("\n");
+
+    return 0;
+}
