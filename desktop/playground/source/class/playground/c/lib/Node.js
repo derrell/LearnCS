37a85159c706ba7f49e0f7cdd8f0e61e6b13dd41
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-05-20 11:10:56 -0400

    do not call initializer on forward function declarations

--- desktop/playground/source/class/playground/c/lib/Node.js
+++ desktop/playground/source/class/playground/c/lib/Node.js
@@ -2624,7 +2624,7 @@ qx.Class.define("playground.c.lib.Node",
                   new playground.c.lib.RuntimeError(
                     this,
                     "Function " + value1.getName() + 
-                    " has been declared, but not defined."));
+                    " has not been defined."));
                 return;
               }
               
@@ -3293,18 +3293,27 @@ qx.Class.define("playground.c.lib.Node",
               // declarators
               data.entry.calculateOffset();
 
-              // Restore data members
-              data.specAndDecl = oldSpecAndDecl;
-              data.typeDeclarators = oldTypeDeclarators;
-
               // If this is a root entry (global variable)...
-              if (data.entry.getSymtab().getParent() === null)
+              if (data.entry.getSymtab().getParent() === null &&
+                  data.entry.getSpecAndDecl()[0].getType() != "function")
               {
                 // ... then initialize it.
-                init_declarator_initialize(success, failure);
+                init_declarator_initialize(
+                  function()
+                  {
+                    // Restore data members
+                    data.specAndDecl = oldSpecAndDecl;
+                    data.typeDeclarators = oldTypeDeclarators;
+                    success();
+                  },
+                  failure);
               }
               else
               {
+                // Restore data members
+                data.specAndDecl = oldSpecAndDecl;
+                data.typeDeclarators = oldTypeDeclarators;
+
                 success();
               }
             }.bind(this),
