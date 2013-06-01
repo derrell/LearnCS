763971ce29aca92caf0cf911638aead253abf65a
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-06-01 17:58:46 -0400

    flag struct bit fields as unsupported; add namespace documentation

--- desktop/playground/source/class/playground/c/lib/Node.js
+++ desktop/playground/source/class/playground/c/lib/Node.js
@@ -31,6 +31,33 @@ if (typeof qx === "undefined" || qx.bConsole)
   require("./NotYetImplemented");
 }
 
+/*
+ * C NAMESPACES
+ * ------------
+ * From http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf
+ *
+ * 6.2.3 Name spaces of identifiers
+ *
+ * If more than one declaration of a particular identifier is visible at
+ * any point in a translation unit, the syntactic context disambiguates uses
+ * that refer   to different entities.
+ *
+ * Thus, there are separate name spaces for various categories of identifiers,
+ * as follows:
+ *
+ * — label names (disambiguated by the syntax of the label declaration and use);
+ *
+ * — the tags of structures, unions, and enumerations (disambiguated by 
+ *   following any32) of the keywords struct, union, or enum);
+ *
+ * — the members of structures or unions; each structure or union has a 
+ *   separate name space for its members (disambiguated by the type of the 
+ *   expression used to access themember via the . or -> operator);
+ *
+ * — all other identifiers, called ordinary identifiers (declared in ordinary 
+ *   declarators or as enumeration constants).
+ */
+
 qx.Class.define("playground.c.lib.Node",
 {
   extend : qx.core.Object,
@@ -4621,6 +4648,14 @@ qx.Class.define("playground.c.lib.Node",
          *   0: declarator?
          *   1: constant_expression (bitfield)
          */
+
+        if (this.children[1].type != "_null_")
+        {
+          failure(new playground.c.lib.RuntimeError(
+                    this,
+                    "Structure bit fields are not supported."));
+          return;
+        }
 
         this.__processSubnodes(
           data,
