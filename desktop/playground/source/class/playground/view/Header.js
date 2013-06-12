2a1a59dbb19797866d3da25e98d44f84eebc0cdc
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-06-12 14:16:18 -0400

    up to version 0.019

--- desktop/playground/source/class/playground/view/Header.js
+++ desktop/playground/source/class/playground/view/Header.js
@@ -40,7 +40,7 @@ qx.Class.define("playground.view.Header",
 
     var version = 
       new qxc.ui.versionlabel.VersionLabel("<i>" + this.tr("LearnCS!") + "</i>",
-                                           "0.018");
+                                           "0.019");
     version.set(
       {
         font : "default",
