c2181479369e02a9ef8a69819ee652f75415cadc
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-06-21 08:17:23 -0400

    add t022 array initializer (not yet working)

---
+++ desktop/compilers/ansic/canning/t022-array-initialize.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char * argv[])
+{
+    int x = 23;
+    char * names[5] = {"no", "first", "second", "third", "fourth"};
+
+    printf("names[4]=%s\n", names[4]);
+    return 0;
+}
