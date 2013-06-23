2b0708057901fbf3e95560c2d11da0bf3892101b
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-06-23 09:09:41 -0400

    test character array initialization from string, too

--- desktop/compilers/ansic/canning/t022-array-initialize.c
+++ desktop/compilers/ansic/canning/t022-array-initialize.c
@@ -7,6 +7,7 @@ int main(int argc, char * argv[])
     int d[] = { 3, 4 };
     char * names5[5] = { "no", "first", "second", "third", "fourth" };
     char * names3[] = { "hi", "there", "world" };
+    char chars[] = "abcde";
 
     printf("a=%d b=%d c[0]=%d c[1]=%d d[0]=%d d[1]=%d\n",
            a, b, c[0], c[1], d[0], d[1]);
@@ -17,6 +18,10 @@ int main(int argc, char * argv[])
     for (a = 0; a < 3; a++)
     {
         printf("names3[%d]=%s\n", a, names3[a]);
+    }
+    for (a = 0; a < 5; a++)
+    {
+        printf("chars[%d]=%c\n", a, chars[a]);
     }
     return 0;
 }
