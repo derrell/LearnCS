204e222bb8d7b5fc6ab0694797cbf8c67cde9ed5
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-06-24 09:30:07 -0400

    more array tests. still working on array initialization

--- desktop/compilers/ansic/canning/t022-array-initialize.c
+++ desktop/compilers/ansic/canning/t022-array-initialize.c
@@ -5,19 +5,24 @@ int main(int argc, char * argv[])
     int b = { 23 };
     int c[2] = { 1, 2 };
     int d[] = { 3, 4 };
-    char * names5[5] = { "no", "first", "second", "third", "fourth" };
-    char * names3[] = { "hi", "there", "world" };
+    char * arr55[5] = { "no", "first", "second", "third", "fourth" };
+    char * arr33[] = { "hi", "there", "world" };
+    char * arr53[5] = { NULL, "one", "two" };
     char chars[] = "abcde";
 
     printf("a=%d b=%d c[0]=%d c[1]=%d d[0]=%d d[1]=%d\n",
            a, b, c[0], c[1], d[0], d[1]);
     for (a = 0; a < 5; a++)
     {
-        printf("names5[%d]=%s\n", a, names5[a]);
+        printf("arr55[%d]=%s\n", a, arr55[a]);
     }
     for (a = 0; a < 3; a++)
     {
-        printf("names3[%d]=%s\n", a, names3[a]);
+        printf("arr33[%d]=%s\n", a, arr33[a]);
+    }
+    for (a = 0; a < 5; a++)
+    {
+        printf("arr53[%d]=%s\n", a, arr53[a]);
     }
     for (a = 0; a < 5; a++)
     {
---
+++ desktop/compilers/ansic/canning/t023-array-initialize-bad-1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, char * argv[])
+{
+    int c[2] = { 1, 2, 3 };
+    return 0;
+}
