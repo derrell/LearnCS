2a56b8bde3a1324bd3d4b531215fb7453a63a5ab
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2013-11-17 11:25:06 -0500

    update test t054 to add additional post-increment testing

--- desktop/compilers/ansic/canning/t054-post-increment-pointer.c
+++ desktop/compilers/ansic/canning/t054-post-increment-pointer.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
+struct X
+{
+    int             x;
+    short           y;
+};
+
 int main(int argc, char * argv[])
 { 
     int *   pInt = 0x100;
     short * pShort = 0x200;
+    struct X * pX = (struct X *) 0x300;
     
     pInt++;
     pShort++;
+    pX++;
+
+    printf("Size of struct X is %d\n", sizeof(struct X));
+    printf("pInt=0x%lx pShort=0x%lx pX=%lx\n", pInt, pShort, pX);
 
     pInt = pInt + 1;
     pShort = pShort + 1;
+    pX = pX + 1;
     
-    printf("pInt=0x%lx pShort=0x%lx\n", pInt, pShort);
+    printf("pInt=0x%lx pShort=0x%lx pX=%lx\n", pInt, pShort, pX);
     return 0;
 }
