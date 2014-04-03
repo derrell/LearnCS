c097503a5aeb052640a0338b9147f9e18459aeeb
user 60 <anonymous@noplace.at.all>
2014-04-03 09:47:42 -0400

    added iscntrl to ctype tests

--- desktop/compilers/ansic/canning/t035-ctype.c
+++ desktop/compilers/ansic/canning/t035-ctype.c
@@ -7,14 +7,15 @@ int main(int argc, char * argv[])
 
     for (c = 0; c < 128; c++)
     {
-        printf("c: 0x%02x %c", c, isprint(c) ? c : ' ');
-        printf(" digit: %d", !! isdigit(c));
-        printf(" space: %d", !! isspace(c));
-        printf(" upper: %d", !! isupper(c));
-        printf(" lower: %d", !! islower(c));
-        printf(" alpha: %d", !! isalpha(c));
-        printf(" print: %d", !! isprint(c));
-        printf(" punct: %d", !! ispunct(c));
+        printf("0x%02x %c", c, isprint(c) ? c : ' ');
+        printf(" digit:%d", !! isdigit(c));
+        printf(" space:%d", !! isspace(c));
+        printf(" upper:%d", !! isupper(c));
+        printf(" lower:%d", !! islower(c));
+        printf(" alpha:%d", !! isalpha(c));
+        printf(" print:%d", !! isprint(c));
+        printf(" punct:%d", !! ispunct(c));
+        printf(" cntrl:%d", !! iscntrl(c));
         printf("\n");
     }
     return 0;
