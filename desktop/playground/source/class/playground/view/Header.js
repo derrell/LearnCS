115ca23f1536ef695b060cc90d4ad037fc182e83
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2014-10-03 14:07:43 -0400

    up to 0.075

--- desktop/playground/source/class/playground/view/Header.js
+++ desktop/playground/source/class/playground/view/Header.js
@@ -55,7 +55,7 @@ qx.Class.define("playground.view.Header",
     qx.core.Init.getApplication().setUserData("whoAmI", whoAmI);
 
     var version = 
-      new qxc.ui.versionlabel.VersionLabel("Playground", "0.074");
+      new qxc.ui.versionlabel.VersionLabel("Playground", "0.075");
     version.set(
       {
         font : "default",
